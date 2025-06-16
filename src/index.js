db8454601b11cb6fe81975a9e277f6442e8871ae
Shipra760 <shahushipra@gmail.com>
2025-06-16 13:59:44 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import "owl.carousel/dist/assets/owl.carousel.css";
-import "owl.carousel/dist/assets/owl.theme.default.css";
-import $ from "jquery";
-window.$ = $;
-window.jQuery = $;
+
 
 
 
